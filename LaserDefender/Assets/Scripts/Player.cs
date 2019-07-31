40fe098064fddcd1b9587e713496a65b5beac6dc
i-z <IvanZdravkovBG@gmail.com>
2019-07-31 23:47:56 +0300

    Player simple moving.

---
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * this.moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * this.moveSpeed;
+
+        this.transform.position = new Vector2(
+            x: this.transform.position.x + deltaX,
+            y: this.transform.position.y + deltaY
+        );
+        
+    }
+}
