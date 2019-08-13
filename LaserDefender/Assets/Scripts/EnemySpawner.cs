4892d99f8372b254413fbe9251518ac968cc4a10
i-z <IvanZdravkovBG@gmail.com>
2019-08-13 20:46:54 +0300

    Enemy Wave spawned.

---
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startingWave = 0;
+    WaveConfig currentWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentWave = this.waveConfigs[this.startingWave];
+
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.NumberOfEnemies; enemyCount++)
+        {
+            Instantiate(
+                original: waveConfig.EnemyPrefab,
+                position: waveConfig.Waypoints[0].transform.position,
+                rotation: Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.TimeBetweenSpawns);
+        }
+    }
+}
