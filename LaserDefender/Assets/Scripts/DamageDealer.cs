db1bf9d2a8a59429af0597b0b44e1a253b8dd788
i-z <IvanZdravkovBG@gmail.com>
2019-08-15 10:51:01 +0300

    Enemies can be destroyed.

---
+++ LaserDefender/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+    
+    public int Damage
+    {
+        get
+        {
+            return this.damage;
+        }
+    }
+
+    public void Hit()
+    {
+        Destroy(this.gameObject);
+    }
+}
