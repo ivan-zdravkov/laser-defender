4f9d374280b0da60edc46f3bf816efdfc03e20a7
i-z <IvanZdravkovBG@gmail.com>
2019-08-15 21:33:34 +0300

    Scrolling Background

---
+++ LaserDefender/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.5f;
+
+    Material material;
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.material = GetComponent<Renderer>().material;
+        this.offset = new Vector2(x: 0f, y: this.scrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.material.mainTextureOffset += this.offset * Time.deltaTime;
+    }
+}
