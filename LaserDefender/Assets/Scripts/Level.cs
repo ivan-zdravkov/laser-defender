be31e27e947220cae6e4afe69a5a256bc9f127a5
i-z <IvanZdravkovBG@gmail.com>
2019-08-19 22:00:20 +0300

    Menu and Game Over.

---
+++ LaserDefender/Assets/Scripts/Level.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void End()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
