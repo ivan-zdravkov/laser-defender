37322ecdc77767969caeaf86072c8c02f84d7127
i-z <IvanZdravkovBG@gmail.com>
2019-08-20 22:55:22 +0300

    More enemy paths.

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -9,10 +9,14 @@ public class EnemyPathing : MonoBehaviour
     private WaveConfig waveConfig;
     private int waypointIndex = 0;
 
+    List<Transform> waypoints;
+    bool startFromEnd;
+    bool reverseY;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = this.waveConfig.Waypoints[this.waypointIndex].transform.position;   
+        this.transform.position = this.waypoints[this.waypointIndex].transform.position;   
     }
 
     // Update is called once per frame
@@ -21,16 +25,27 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
-    public void SetWaveConfig(WaveConfig waveConfig)
+    public void SetWaveConfig(WaveConfig waveConfig, bool startFromEnd, bool reverseY)
     {
         this.waveConfig = waveConfig;
+        this.startFromEnd = startFromEnd;
+        this.reverseY = reverseY;
+
+        this.waypoints = this.waveConfig.Waypoints;
+
+        if (this.startFromEnd)
+            this.waypoints.Reverse();
+
+        if (this.reverseY)
+            foreach (Transform waypoint in waypoints)
+                waypoint.position.Set(waypoint.position.x, -waypoint.position.y, waypoint.position.z);
     }
 
     private void Move()
     {
-        if (this.waypointIndex < this.waveConfig.Waypoints.Count - 1)
+        if (this.waypointIndex < this.waypoints.Count - 1)
         {
-            Vector3 targetPosition = this.waveConfig.Waypoints[this.waypointIndex + 1].transform.position;
+            Vector3 targetPosition = this.waypoints[this.waypointIndex + 1].transform.position;
 
             transform.position = Vector3.MoveTowards(
                 current: this.transform.position,
--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -35,14 +35,24 @@ public class EnemySpawner : MonoBehaviour
     {
         waveConfig.SetDificulty(this.difficulty);
 
+        bool startFromEnd = UnityEngine.Random.Range(0, 2) == 0;
+        bool reverseY = UnityEngine.Random.Range(0, 2) == 0;
+
         for (int enemyCount = 0; enemyCount < waveConfig.NumberOfEnemies; enemyCount++)
         {
+            int index = startFromEnd ? waveConfig.Waypoints.Count - 1 : 0;
+
             GameObject newEnemy = Instantiate(
                 original: waveConfig.EnemyPrefab,
-                position: waveConfig.Waypoints[0].transform.position,
+                position: new Vector3(
+                    x: waveConfig.Waypoints[index].transform.position.x,
+                    y: reverseY ?
+                        -waveConfig.Waypoints[index].transform.position.y :
+                        waveConfig.Waypoints[index].transform.position.y,
+                    z: waveConfig.Waypoints[index].transform.position.z),
                 rotation: Quaternion.identity);
 
-            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig, startFromEnd, reverseY);
 
             yield return new WaitForSeconds(waveConfig.TimeBetweenSpawns);
         }
